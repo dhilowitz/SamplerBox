ef909b835210c3399e53fa15c736fc76692b1117
David Hilowitz <dhilowitz@gmail.com>
2016-11-10 11:34:00 -0500

    Updated setup.py so that it works on Mac

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
+import os
 
+os.environ["CPPFLAGS"] = os.getenv("CPPFLAGS", "") + "-I" + numpy.get_include() 
 setup(ext_modules = cythonize("samplerbox_audio.pyx"), include_dirs=[numpy.get_include()])
